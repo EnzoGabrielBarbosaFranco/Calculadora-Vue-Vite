29b4625cbbd7afb4dfc73fd24b15c827b78c0fe0
Enzo Gabriel <enzoddos7@gmail.com>
2023-10-23 18:59:56 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# calculator
+
+Simple calculator app build with javascript
+
+[Live Demo](https://calculator-vert-beta.vercel.app/)
+
+## Stack
+- [VueJS](https://vuejs.org/)
+-	[Vite](https://vitejs.dev/)
+- [Tailwindcss](https://tailwindcss.com/)
+
+<!-- ### TODO -->
+
+<!-- - fix 'Octal literals are not allowed in strict mode.' example: '21+06'
+- add plus/minur number (auto bracket)
+- add gradient on top display (first application clash with scroll element) -->
---
+++ src/components/NavigatorMenu.vue
@@ -0,0 +1,40 @@
+<script>
+  import CalculatorIcon from './icons/CalculatorIcon.vue'
+  import GithubIcon from './icons/GithubIcon.vue'
+  import TrashIcon from './icons/TrashIcon.vue'
+
+  export default {
+    name: 'NavigatorMenu',
+    components: { CalculatorIcon, GithubIcon, TrashIcon },
+		props: {
+			deleteHistory: Function
+		}
+  }
+</script>
+<template>
+  <div class="flex items-center justify-between py-2 rounded-t-md">
+    <div class="flex space-x-2">
+      <div class="menu menu-active">
+        <calculator-icon class="text-blue-500" />
+        <p class="text-sm font-medium text-blue-500">Calculadora</p>
+      </div>
+      <a href="https://github.com/EnzoGabrielBarbosaFranco" target="_blank" class="menu">
+        <github-icon class="" />
+        <p class="text-sm font-medium">Github</p>
+      </a>
+    </div>
+    <div class="menu">
+      <trash-icon @click="deleteHistory" />
+    </div>
+  </div>
+</template>
+
+<style lang="postcss">
+  .menu {
+    @apply flex items-center space-x-0.5 bg-slate-900 text-blue-800 hover:text-blue-500 hover:bg-slate-700 px-1.5 py-1 rounded cursor-pointer;
+  }
+
+  .menu-active {
+    @apply bg-slate-700;
+  }
+</style>
